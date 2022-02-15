a730b9cbb3da84d39ca20fef29a6baa878094359
NikitaFeaucher <nb49060@gmail.com>
2022-02-15 18:57:14 +0200

    4735

---
+++ JavaScript/Wellness.js
@@ -0,0 +1,5 @@
+//Комментарии в JavaScript по синтаксису не отличаются от языков программирования: Pascal, C++, C#, C и т.д.
+function CaloriesCounting()
+{
+    //
+}
---
+++ PublicCommit.bat
@@ -0,0 +1,7 @@
+git init
+git remote add origin https://github.com/NikitaFeaucher/Wellness.git
+git add .
+git commit -m %RANDOM%
+git branch -M main
+git push -u origin main
+pause
